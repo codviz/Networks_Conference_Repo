5aa7da90c1a286c7cec5ecca2abe0f4a9b90ccf0
codviz <89945886+codviz@users.noreply.github.com>
2024-10-07 19:04:03 -0400

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,18 +1,28 @@
 # echo-server
 
 import socket
+import random
 
 HOST = "127.0.0.1"
 PORT = 65432
+guess = str(random.randint(0,101))
+correct_message = bytes("You Got It!", encoding = 'ascii')
+incorrect_message = bytes("Wrong Guess!", encoding = 'ascii')
+severing_message = bytes("Ending Connection...", encoding = 'ascii')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = (b"Sending The Name Back")
-            if not data:
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            data = conn.recv(1024)
+            if data == bytes(guess, encoding = 'ascii'):
+                conn.sendall(correct_message)
                 break
-            conn.sendall(data)
+            if data == bytes('-1', encoding = 'ascii'):
+                conn.sendall(severing_message)
+                break
+            conn.sendall(incorrect_message)
+
