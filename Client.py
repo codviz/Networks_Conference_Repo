175df6a1ef0f14c71a90677e06f3ed498eca2203
codviz <89945886+codviz@users.noreply.github.com>
2024-10-07 19:03:46 -0400

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,10 +5,13 @@ import socket
 HOST = "127.0.0.1"
 PORT = 65432  
 
+Hello = bytes('Hello', encoding = 'ascii')
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    print("Sending Data...")
-    s.sendall(b"My Name")
+    data_string = input("Guess A Number Between 1 and 100 (enter -1 to quit at anytime): ")
+    data = bytes(data_string, encoding = 'ascii')
+    s.sendall(data)
     data = s.recv(1024)
 
 print(f"Received {data!r}")
