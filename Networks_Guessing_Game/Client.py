6cb546c23f0d05cf43abdc38f2d9512f72786d80
codviz <89945886+codviz@users.noreply.github.com>
2024-11-01 16:42:33 -0400

    Update Client.py

--- Networks_Guessing_Game/Client.py
+++ Networks_Guessing_Game/Client.py
@@ -1,1 +1,44 @@
+import socket, threading
 
+def handle_messages(connection: socket.socket):
+
+    while True:
+        try:
+            msg = connection.recv(1024)
+            print(msg.decode())
+
+        except Exception as e:
+            print(f'Error handling message from server: {e}')
+            connection.close()
+            break
+
+def client():
+    
+    SERVER_ADDRESS = '127.0.0.1'
+    SERVER_PORT = 12000
+
+    try:
+        socket_instance = socket.socket()
+        socket_instance.connect((SERVER_ADDRESS, SERVER_PORT))
+        threading.Thread(target=handle_messages, args=[socket_instance]).start()
+
+        print('Connected To The Server!')
+        print("Guess A Number Between 0 and 4, Type 'quit' to exit: ")
+
+        while True:
+            msg = input()
+
+            if msg == 'quit':
+                break
+
+            socket_instance.send(msg.encode())
+            
+        socket_instance.close()
+
+    except Exception as e:
+        print(f'Error connecting to server socket {e}')
+        socket_instance.close()
+
+
+if __name__ == "__main__":
+    client()
